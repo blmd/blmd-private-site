4e0912dcbf8cc4703b462b6abff7ec39cd4430b5
Casey <caseyjvcain@gmail.com>
2015-09-28 20:40:31 -0400

    add github updater header

--- blmd-private-site.php
+++ blmd-private-site.php
@@ -1,12 +1,20 @@
 <?php
 /*
 Plugin Name: BLMD Private Site
-Plugin URI: http://github.com/blmd/blmd-private-site
+Plugin URI: https://github.com/blmd/blmd-private-site
 Description: Require login in order to view site
 Author: blmd
-Author URI: http://github.com/blmd
-Version: 0.1
+Author URI: https://github.com/blmd
+Version: 0.2
+
+GitHub Plugin URI: https://github.com/blmd/blmd-private-site
 */
+
+!defined( 'ABSPATH' ) && die;
+define( 'BLMD_PRIVATE_SITE_VERSION', '0.2' );
+define( 'BLMD_PRIVATE_SITE_URL', plugin_dir_url( __FILE__ ) );
+define( 'BLMD_PRIVATE_SITE_DIR', plugin_dir_path( __FILE__ ) );
+define( 'BLMD_PRIVATE_SITE_BASENAME', plugin_basename( __FILE__ ) );
 
 add_filter( 'pre_option_blog_public', '__return_zero' );
 add_action( "parse_request", function() {
